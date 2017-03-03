a6fe4608cb004ed8c2e26c1be2c2322eed06fa3f
Florian <Silverwing@illarion.org>
2017-03-03 14:50:34 +0100

    New stuff

---
+++ Illarion/Kampfsystem/chanceToHit.m
@@ -0,0 +1,15 @@
+function [ hitChance, hitFlag ] = chanceToHit( character, weapon )
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+    hitFlag = false;
+    weaponAccuracy = weapon.wp_accuracy;
+    perception = character.attributes.perception;
+    
+    hitChance = min(5,max(95, weaponAccuracy*(1+(perception-10)/500)));
+    
+    if rand*100<= hitChance 
+        hitFlag = true;
+    end
+    
+end
+
